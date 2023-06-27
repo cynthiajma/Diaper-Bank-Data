3a530829dfd76ed6db517e66d34aa9d269f0d905
ssw47 <122422671+ssw47@users.noreply.github.com>
2023-06-27 11:11:10 -0400

    Add files via upload

---
+++ diaperdata.py
@@ -0,0 +1,58 @@
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
+import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
+
+df = pd.read_csv("diaperdata.csv",encoding="latin-1")
+
+df['State'] = df['State'].replace([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25], ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO'])
+df['State'] = df['State'].replace([26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 99], ['MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY', "Multiple States"])
+
+df.loc[(df['DiaperBankName'] == 'Bare Needs Diaper Bank'), 'State'] = 'TN'
+df.loc[(df['DiaperBankName'] == 'Basic Necessities'), 'State'] = 'LA'
+df.loc[(df['DiaperBankName'] == 'Cradles to Crayons Philadelphia'), 'State'] = 'PA'
+df.loc[(df['DiaperBankName'] == 'Diaper Bank of the Ozarks'), 'State'] = 'MO'
+df.loc[(df['DiaperBankName'] == 'Greater Philadelphia Diaper Bank'), 'State'] = 'PA'
+df.loc[(df['DiaperBankName'] == 'HappyBottoms'), 'State'] = 'MO'
+df.loc[(df['DiaperBankName'] == 'Helping Mamas'), 'State'] = 'GA'
+df.loc[(df['DiaperBankName'] == 'Keeping Families Covered'), 'State'] = 'IL'
+df.loc[(df['DiaperBankName'] == 'Sweet Cheeks Diaper Bank'), 'State'] = 'OH'
+df.loc[(df['DiaperBankName'] == 'Sweet Cheeks Diaper Ministry'), 'State'] = 'TN'
+df.loc[(df['DiaperBankName'] == 'The Life House'), 'State'] = 'NE'
+
+df.loc[(df['CensusRegion'] == 1), 'CensusRegion'] = 'Northeast'
+df.loc[(df['CensusRegion'] == 2), 'CensusRegion'] = 'Middle Atlantic'
+df.loc[(df['CensusRegion'] == 3), 'CensusRegion'] = 'South'
+df.loc[(df['CensusRegion'] == 4), 'CensusRegion'] = 'West'
+
+df.loc[(df['DB_Transport'] == 1), 'DB_Transport'] = 'Walk'
+df.loc[(df['DB_Transport'] == 2), 'DB_Transport'] = 'Public Transportation'
+df.loc[(df['DB_Transport'] == 3), 'DB_Transport'] = 'Drove Self'
+df.loc[(df['DB_Transport'] == 4), 'DB_Transport'] = 'Got a Ride'
+df.loc[(df['DB_Transport'] == 5), 'DB_Transport'] = 'Taxi/Ride Sharing App'
+
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.H1(children='Diaper Bank Data', style={'textAlign':'center'}),
+    dcc.Dropdown(df.CensusRegion.unique(), 'Northeast', id='dropdown-selection'),
+    dcc.Graph(id='graph-content')
+])
+
+@callback(
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value')
+)
+def update_graph(value):
+    dff = df[df.CensusRegion==value]
+    dff = dff[["DB_Transport"]].value_counts()
+    dff = dff.to_frame().reset_index()
+    dff.rename(columns={0: 'Count'}, inplace=True)
+    dff.rename(columns={"DB_Transport": 'Method'}, inplace=True)
+    px.bar(dff, x="DB_Transport", y="Count")
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
