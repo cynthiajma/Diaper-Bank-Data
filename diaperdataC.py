70ff9d18e86b6e2cf61f916a5dbd39d31386d3c4
Cynthia Ma <majcynthia@gmail.com>
2023-07-26 15:38:34 -0400

    added stuff

--- diaperdataC.py
+++ diaperdataC.py
@@ -9,6 +9,7 @@ import plotly.graph_objects as go
 
 df = pd.read_csv("diaperdata.csv", encoding="latin-1")
 
+#Reading and opening census data
 acsincome = pd.read_csv("ACS_5year_2021_income.csv", encoding="latin-1")
 acsincome = acsincome.loc[acsincome['Race'] != 'ALL']
 acsincome = acsincome.loc[acsincome['Race'] != 'WHITE']
@@ -21,7 +22,7 @@ acsincome['variable'] = acsincome['variable'].replace(["<16k", "16-20k", "20-25k
                                                       ['<=15,999', '16,000-19,999', '20,000-24,999', '25,000-29,999',
                                                      '30,000-34,999', '35,000-39,999', '40,000-44,999', '45,000-49,999',
                                                      '50,000-59,999', '60,000-69,999', '70,000-79,999', '>=80,000'])
-
+#Renaming race values in census data
 acsincome.loc[(acsincome['Race'] == 'ASIAN'), 'Race'] = 'Asian'
 acsincome.loc[(acsincome['Race'] == 'BLACK OR AFRICAN AMERICAN'), 'Race'] = 'Black'
 acsincome.loc[(acsincome['Race'] == 'AMERICAN INDIAN AND ALASKA NATIVE'), 'Race'] = 'American Indian or Alaskan Native'
@@ -30,6 +31,7 @@ acsincome.loc[(acsincome['Race'] == 'TWO OR MORE RACES'), 'Race'] = 'Multiracial
 acsincome.loc[(acsincome['Race'] == 'HISPANIC OR LATINO'), 'Race'] = 'Hispanic'
 acsincome.loc[(acsincome['Race'] == 'WHITE, NOT HISPANIC OR LATINO'), 'Race'] = 'White'
 
+#Replacing all state values for initials in census data
 acsincome.loc[(acsincome['State'] == 'Alabama'), 'State'] = 'AL'
 acsincome.loc[(acsincome['State'] == 'Alaska'), 'State'] = 'AK'
 acsincome.loc[(acsincome['State'] == 'Arizona'), 'State'] = 'AZ'
@@ -82,6 +84,7 @@ acsincome.loc[(acsincome['State'] == 'West Virginia'), 'State'] = 'WV'
 acsincome.loc[(acsincome['State'] == 'Wisconsin'), 'State'] = 'WI'
 acsincome.loc[(acsincome['State'] == 'Wyoming'), 'State'] = 'WY'
 
+#Replacing all state values with state intiials
 df['State'] = df['State'].replace([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
                                    24, 25], ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID',
                                              'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS',
@@ -91,6 +94,7 @@ df['State'] = df['State'].replace([26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 3
                                                          'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT',
                                                          'VA', 'WA', 'WV', 'WI', 'WY', "Multiple States"])
 
+#Adding additional diaper bank state info
 df.loc[(df['DiaperBankName'] == 'Bare Needs Diaper Bank'), 'State'] = 'TN'
 df.loc[(df['DiaperBankName'] == 'Basic Necessities'), 'State'] = 'LA'
 df.loc[(df['DiaperBankName'] == 'Cradles to Crayons Philadelphia'), 'State'] = 'PA'
@@ -104,11 +108,7 @@ df.loc[(df['DiaperBankName'] == 'Sweet Cheeks Diaper Ministry'), 'State'] = 'TN'
 df.loc[(df['DiaperBankName'] == 'The Life House'), 'State'] = 'NE'
 df.loc[(df['DiaperBankName'] == 'Emergency Infant Services'), 'State'] = 'OK'
 
-df.loc[(df['CensusRegion'] == 1), 'CensusRegion'] = 'Northeast'
-df.loc[(df['CensusRegion'] == 2), 'CensusRegion'] = 'Middle Atlantic'
-df.loc[(df['CensusRegion'] == 3), 'CensusRegion'] = 'South'
-df.loc[(df['CensusRegion'] == 4), 'CensusRegion'] = 'West'
-
+#Replacing values for transport type
 df.loc[(df['DB_Transport'] == 1), 'DB_Transport'] = 'Walk'
 df.loc[(df['DB_Transport'] == 2), 'DB_Transport'] = 'Public Transportation'
 df.loc[(df['DB_Transport'] == 3), 'DB_Transport'] = 'Drove Self'
@@ -116,6 +116,7 @@ df.loc[(df['DB_Transport'] == 4), 'DB_Transport'] = 'Got a Ride'
 df.loc[(df['DB_Transport'] == 5), 'DB_Transport'] = 'Taxi/Ride Sharing App'
 df.loc[(df['PartnerAgencyType'] == "HOMEVISIT"), 'DB_Transport'] = 'Home Visit'
 
+#Replacing values for race
 df.loc[(df['Race_PreferNoShare'] == 1), 'Race'] = 'Prefer Not to Share'
 df.loc[(df['Race_AIAN'] == 1), 'Race'] = 'American Indian or Alaskan Native'
 df.loc[(df['Race_Asian'] == 1), 'Race'] = 'Asian'
@@ -130,6 +131,21 @@ df.loc[((df[['Race_PreferNoShare', 'Race_AIAN', 'Race_Asian', 'Race_BlackAA', 'R
 df.loc[((df[['Race_PreferNoShare', 'Race_AIAN', 'Race_Asian', 'Race_BlackAA', 'Race_Hispanic', 'Race_NativeHawaiianPI',
              'Race_White', 'Race_MENA', 'Race_Multiracial']].sum(axis=1)) == 0), 'Race'] = 'Prefer Not to Share'
 
+#Replacing values for education type graph
+df.loc[(df['Ad1_EduType'] == 1), 'Ad1_EduType'] = 'High School'
+df.loc[(df['Ad1_EduType'] == 2), 'Ad1_EduType'] = 'GED'
+df.loc[(df['Ad1_EduType'] == 3), 'Ad1_EduType'] = 'Associate\'s/2-year'
+df.loc[(df['Ad1_EduType'] == 4), 'Ad1_EduType'] = 'Bachelor\'s/4-year'
+df.loc[(df['Ad1_EduType'] == 5), 'Ad1_EduType'] = 'Graduate Degree'
+df.loc[(df['Ad1_EduType'] == 6), 'Ad1_EduType'] = 'Enrolled in a job-training/non-degree program'
+df.loc[(df['Ad2_EduType'] == 1), 'Ad2_EduType'] = 'High School'
+df.loc[(df['Ad2_EduType'] == 2), 'Ad2_EduType'] = 'GED'
+df.loc[(df['Ad2_EduType'] == 3), 'Ad2_EduType'] = 'Associate\'s/2-year'
+df.loc[(df['Ad2_EduType'] == 4), 'Ad2_EduType'] = 'Bachelor\'s/4-year'
+df.loc[(df['Ad2_EduType'] == 5), 'Ad2_EduType'] = 'Graduate Degree'
+df.loc[(df['Ad2_EduType'] == 6), 'Ad2_EduType'] = 'Enrolled in a job-training/non-degree program'
+
+#Assigning 2020 state median information from FRED data
 df.loc[(df['State'] == 'AL'), 'State_2020_Median'] = 57243
 df.loc[(df['State'] == 'CA'), 'State_2020_Median'] = 81278
 df.loc[(df['State'] == 'CO'), 'State_2020_Median'] = 87689
@@ -160,22 +176,24 @@ df.loc[(df['State'] == 'UT'), 'State_2020_Median'] = 87915
 df.loc[(df['State'] == 'WA'), 'State_2020_Median'] = 85157
 df.loc[(df['State'] == 'WY'), 'State_2020_Median'] = 68506
 
+#Replacing values for single household
 df.loc[(df['NumAdults'] == 1), 'Single Household'] = 'Yes'
 df.loc[(df['NumAdults'] != 1), 'Single Household'] = 'No'
 singlehead = ['Yes', 'No']
 
+#Sorting states, setting races
 states = df["State"].sort_values().unique()
-regions = df["CensusRegion"].sort_values().unique()
 races = ['American Indian or Alaskan Native', 'Asian', 'Black', 'Hispanic', 'Middle Eastern or North African',
          'Native Hawaiian or Pacific Islander', 'White', 'Multiracial', 'Prefer Not to Share']
-
 df['Income_2020_2'] = df['Income_2020']
 
+#creating filters dictionary to add filters to
 filters = {"race": "",
            "region": "", "state": "", 'singlehead': ""}
 
 global percent_inHome
 
+#app layout
 external_stylesheets = ['assets/diaperstyles.css']
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 app.title = "Diaper Bank Household Data"
@@ -273,12 +291,11 @@ app.layout = html.Div(
             children=[
                 dcc.Graph(id='map-content'),
                 html.H3(children='Additional Visualizations', className='part2-header'),
-                html.Br(),
                 html.Div(
                     children=[
                         html.H3(children="How Diaper Bank Recipients Access Diaper Bank Products",
-                                className='smaller-title'),
-                        html.Div(id='display-selected-filtersTRANSPORT', className='smaller-title'),
+                                className='graph-title'),
+                        html.Div(id='display-selected-filtersTRANSPORT', className='subtitle'),
                         dcc.Graph(id='transport-content', className='transport1'),
                         dcc.Graph(id='transport-pie-content', className='transport2')
                     ],
@@ -287,17 +304,18 @@ app.layout = html.Div(
                 html.Div([
                     html.Div(
                         children=[
-                            html.H3(children="Childcare Use", className='small-title'),
-                            html.Div(id='display-selected-filtersCHILDCARE', className='smaller-title'),
+                            html.H3(children="Childcare Use", className='graph-title'),
+                            html.Div(id='display-selected-filtersCHILDCARE', className='subtitle'),
                             dcc.Graph(id='childcare1-content', className='childgraph'),
                             dcc.Graph(id='childcare2-content', className='childgraph')
                         ],
                     ),
+                    html.Br(),
                     html.Div(
                         children=[
-                            html.H3(children="Distribution of Income for Diaper Bank Recipients vs ACS 5-Year Survey",
-                            className='small-title'),
-                            html.Div(id='display-selected-filtersINCOME', className='smaller-title'),
+                            html.H3(children="Comparison of Income Distributions for Diaper Bank Households and U.S. Census Data",
+                            className='graph-title'),
+                            html.Div(id='display-selected-filtersINCOME', className='subtitle'),
                             dcc.Graph(id='income2019-content', className='incomegraphs'),
                             dcc.Graph(id='income2020-content', className='incomegraphs')
                         ],
@@ -306,8 +324,8 @@ app.layout = html.Div(
                     html.Div(
                         children=[
                             html.H3(children="Effect of Receiving Diapers on Various Diaper Illnesses",
-                                    className='small-title'),
-                            html.Div(id='display-selected-filtersILLNESS', className='smaller-title'),
+                                    className='graph-title'),
+                            html.Div(id='display-selected-filtersILLNESS', className='subtitle'),
                             dcc.Graph(id='DR-content', className='sankeygraphs'),
                             dcc.Graph(id='SevDR-content', className='sankeygraphs'),
                             dcc.Graph(id='UTI-content', className='sankeygraphs')
@@ -677,42 +695,6 @@ def display_choropleth(variable, race, state, singlehead):
         return fig
 
 
-@callback(
-   Output('transport-content', 'figure'),
-   [Input('race', 'value'),
-    Input('state', 'value'),
-    Input('singlehead', 'value')])
-def update_transport_graph(race, state, singlehead):
-    filters["race"] = race if race else ""
-    filters['state'] = state if state else ""
-    filters['singlehead'] = singlehead if singlehead else ""
-    dff = pd.DataFrame()
-
-    if 'race' in filters or 'state' in filters or 'singlehead' in filters:
-        dff = df[['Race', 'State', 'Single Household', 'DB_Transport']]
-        if filters['race']:
-            dff = dff.loc[dff['Race'] == filters['race']]
-        if filters['state']:
-            dff = dff.loc[dff['State'] == filters['state']]
-        if filters['singlehead']:
-            dff = dff.loc[dff['Single Household'] == filters['singlehead']]
-
-    dff = dff.sort_values('DB_Transport')
-    fig = px.histogram(dff, x="DB_Transport",
-                       labels={
-                           "DB_Transport": "Method",
-                           "count": "Count"},
-                       template='plotly_white',
-                       title="How Diaper Bank Recipients Access Diaper Bank Products<br><sup>You have selected "
-                             + str(race) + " as race, " + str(state) + " as state, and " + str(singlehead)
-                             + " for single head household.",
-                       category_orders={"DB_Transport": ["Drove Self", "Got a Ride", "Public Transportation",
-                                                         "Taxi/Ride Sharing App", "Walk", "Home Visit"
-                                                         ]}
-                       )
-    fig.update_layout(yaxis_title="Count")
-    fig.update_traces(marker_color='#86bce8')
-    return fig
 
 @callback(
    Output('transport-pie-content', 'figure'),
@@ -721,43 +703,7 @@ def update_transport_graph(race, state, singlehead):
     Input('singlehead', 'value')]
 )
 def update_transport_pie(race, state, singlehead):
-    filters["race"] = race if race else ""
-    filters['state'] = state if state else ""
-    filters['singlehead'] = singlehead if singlehead else ""
-    dff = pd.DataFrame()
-
-    if 'race' in filters or 'state' in filters or 'singlehead' in filters:
-        dff = df[['Race', 'State', 'Single Household', 'DB_Transport']]
-        if filters['race']:
-            dff = dff.loc[dff['Race'] == filters['race']]
-        if filters['state']:
-            dff = dff.loc[dff['State'] == filters['state']]
-        if filters['singlehead']:
-            dff = dff.loc[dff['Single Household'] == filters['singlehead']]
-
-    dff = dff.dropna()
-    fig = px.pie(dff, names="DB_Transport",
-                 category_orders={"DB_Transport": ["Drove Self", "Got a Ride", "Public Transportation", "Taxi/Ride",
-                                                   "Sharing App", 'Walk']},
-                 labels={"DB_Transport": "Method"},
-                 template="plotly_white",
-                 color_discrete_sequence=px.colors.sequential.RdBu_r)
-    nrows = dff.shape[0]
-    fig.update_layout(annotations=[dict(
-        x=0.5,
-        y=-0.19,
-        xref='paper',
-        yref='paper',
-        text=f'Filters match to {nrows} responses.',
-        showarrow=False
-    )])
-    fig.update_layout(
-        title_font=dict(
-            family='Montserrat',
-            size=21,
-            color='black'
-        ))
-    return fig
+    
 
 
 @callback(
@@ -984,7 +930,7 @@ def update_income2019(race, state, singlehead):
     acs = acs.groupby('variable').sum()
     acs['Percentage'] = acs['value'] / acs['value'].sum() * 100
     acs = acs.rename_axis('Income Range').reset_index()
-    acs['Type'] = 'National Data'
+    acs['Type'] = 'ACS 5-Year Survey'
     dff['Type'] = 'Diaper Bank Recipients'
 
     dff = dff[['Income Range', 'Percentage', 'Type']]
@@ -998,9 +944,7 @@ def update_income2019(race, state, singlehead):
                                                          '25,000-29,999', '30,000-34,999', '35,000-39,999',
                                                          '40,000-44,999', '45,000-49,999', '50,000-59,999',
                                                          '60,000-69,999', '70,000-79,999', '>=80,000']},
-                       title="Distribution of Household Incomes in 2019<br><sup>You have selected "
-                             + str(race) + " as race, " + str(state) + " as state, and " + str(singlehead)
-                             + " for single head household.",
+                       title="2019",
                        template='plotly_white')
     fig.update_layout(
         title_font=dict(
@@ -1020,7 +964,7 @@ def update_income2019(race, state, singlehead):
     fig.update_traces(opacity=0.40)
     fig.update_layout(annotations=[dict(
         x=0.5,
-        y=-0.4,
+        y=1,
         xref='paper',
         yref='paper',
         text=f'Filters matched to {rows} responses.',
@@ -1067,7 +1011,7 @@ def update_income2020(race, state, singlehead):
     acs = acs.groupby('variable').sum()
     acs['Percentage'] = acs['value'] / acs['value'].sum() * 100
     acs = acs.rename_axis('Income Range').reset_index()
-    acs['Type'] = 'National Data'
+    acs['Type'] = 'ACS 5-Year Survey'
     dff['Type'] = 'Diaper Bank Recipients'
 
     dff = dff[['Income Range', 'Percentage', 'Type']]
@@ -1081,9 +1025,7 @@ def update_income2020(race, state, singlehead):
                                                          '25,000-29,999', '30,000-34,999', '35,000-39,999',
                                                          '40,000-44,999', '45,000-49,999', '50,000-59,999',
                                                          '60,000-69,999', '70,000-79,999', '>=80,000']},
-                       title="Distribution of Household Incomes in 2020<br><sup>You have selected "
-                             + str(race) + " as race, " + str(state) + " as state, and " + str(singlehead)
-                             + " for single head household.",
+                       title="2020",
                        template='plotly_white')
     fig.update_layout(
         title_font=dict(
@@ -1102,7 +1044,7 @@ def update_income2020(race, state, singlehead):
     fig.update_traces(opacity=0.40)
     fig.update_layout(annotations=[dict(
         x=0.5,
-        y=-0.25,
+        y=1,
         xref='paper',
         yref='paper',
         text=f'Filters matched to {rows} responses.',
